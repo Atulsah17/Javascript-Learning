a2a4dfe48ae2b916e2a2f0a360a489ae1222dd91
Atul Sah <atulsah9211@gmail.com>
2023-11-22 23:50:39 +0530

    first commit

---
+++ 01_conversionsOperation.js
@@ -0,0 +1,12 @@
+//  let score= "33abc "
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log((valueInNumber));
+
+ let someNumber =33 
+  let stringNumber = String(someNumber)
+  console.log(typeof stringNumber);
+  console.log(stringNumber);
---
+++ o1_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "atul@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+// accountId =2
+console.log (accountId);
+ accountEmail = "hbxsvsd.com"
+ accountPassword = "273632"
+ accountCity = "china"
+ console.log (accountId);
+ console.table([accountId, accountEmail, accountPassword, accountCity])
