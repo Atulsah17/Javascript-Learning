fd878b62f8c2d4132d72f10def0e807e087b5f87
Atul Sah <atulsah9211@gmail.com>
2023-12-01 23:24:58 +0530

    second commit

--- 01_conversionsOperation.js
+++ 01_conversionsOperation.js
@@ -6,7 +6,27 @@
 //  console.log(typeof valueInNumber);
 //  console.log((valueInNumber));
 
- let someNumber =33 
-  let stringNumber = String(someNumber)
-  console.log(typeof stringNumber);
-  console.log(stringNumber);
+//  let someNumber =33 
+//   let stringNumber = String(someNumber)
+//   console.log(typeof stringNumber);
+//   console.log(stringNumber); 
+// let userEmail;
+// const id = Symbol ('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+let myYoutubename = "vvgdvsgdhbhdhhdddbd"
+let anotherName = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@googel.com",
+    upi : "user@ybl"
+
+}
+let userTwo = userOne
+userTwo.email = "bhsbhbssjss"
+console.log(userOne.email);
+console.log(userTwo.email);
---
+++ 05_strings.js
@@ -0,0 +1,14 @@
+const name = "atul"
+const repoCount = 50
+// console.log(name + repoCount + "value");
+console.log (`hello my is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Atul")
+console.log(gameName[0]);
+console.log(gameName[4]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0 , 2)
+console.log(newString);
